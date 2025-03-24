0fe9f8364f106246240bd2fdf8e756e1661a9dca
sohmatimoto <132068329+sohmatimoto@users.noreply.github.com>
2025-03-24 10:44:11 -0300

    Add files via upload

---
+++ tropicadelia2-main/tropicalia-2025-main/tropicalia-2025-main/site-acessivel-projeto-inicial/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function(){
+   const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+   const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
+
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+    const altenaContraste = document.getElementById(alterna-contraste)
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    altenaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+    })
+
+    
+})
